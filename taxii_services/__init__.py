9a07b3708966a929987122b6e85a5cb8621b44cf
MarkDavidson <mdavidson@mitre.org>
2014-09-02 08:23:48 -0400

    modifications

--- taxii_services/__init__.py
+++ taxii_services/__init__.py
@@ -7,6 +7,9 @@ def register_admins(admin_list=None):
     import admin
     admin.register_admins(admin_list)
 
+# TODO: Calling this function borks loaddata with the following error:
+# IntegrityError: Problem installing fixture 'yeti\fixtures\initial_data.json': Could not load taxii_services._Handler(pk=1): column
+ # handler is not unique
 def register_message_handlers(handler_list=None):
     """
     Args:
