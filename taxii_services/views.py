375540aab59a86156240d9a582c999ff6de5cfc5
MarkDavidson <mdavidson@mitre.org>
2014-09-08 07:58:30 -0400

    Added ParseTuple concept, other cleanup

--- taxii_services/views.py
+++ taxii_services/views.py
@@ -12,6 +12,7 @@ import libtaxii.messages_10 as tm10
 from libtaxii.constants import *
 from libtaxii.validation import SchemaValidator
 
+import collections
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from lxml.etree import XMLSyntaxError
@@ -19,6 +20,14 @@ from StringIO import StringIO
 import traceback
 import sys
 from importlib import import_module
+
+ParseTuple = collections.namedtuple('ParseTuple', ['validator', 'parser']')
+
+TAXII_10_ParseTuple = ParseTuple(SchemaValidator(SchemaValidator.TAXII_10_SCHEMA), tm10.get_message_from_xml)
+TAXII_11_ParseTuple = ParseTuple(SchemaValidator(SchemaValidator.TAXII_11_SCHEMA), tm11.get_message_from_xml)
+
+xtct_map = {VID_TAXII_XML_10: TAXII_10_ParseTuple,
+            VID_TAXII_XML_11: TAXII_11_ParseTuple }
 
 @csrf_exempt
 def service_router(request, path, do_validate=True):
@@ -29,48 +38,46 @@ def service_router(request, path, do_validate=True):
     
     if request.method != 'POST':
         raise StatusMessageException('0', ST_BAD_MESSAGE, 'Request method was not POST!')
-    
+
     xtct = request.META.get('HTTP_X_TAXII_CONTENT_TYPE', None)
-    # TODO: Map/dict is probably a better use than if/else
-    if xtct == VID_TAXII_XML_10:
-        sv = SchemaValidator(SchemaValidator.TAXII_10_SCHEMA)
-        get_message_from_xml = tm10.get_message_from_xml
-    else: # assume xtct == VID_TAXII_XML_11, since the headers have been validated #TODO: This statement is not true
-        sv = SchemaValidator(SchemaValidator.TAXII_11_SCHEMA)
-        get_message_from_xml = tm11.get_message_from_xml
-    
+    if not xtct:
+        raise StatusMessageException('0', ST_BAD_MESSAGE, 'The X-TAXII-Content-Type Header was not present.')
+
+    parse_tuple = xtct_map.get(xtct)
+    if not parse_info:
+        raise StatusMessageException('0', ST_BAD_MESSAGE, 'The X-TAXII-Content-Type Header is not supported.')
+
     if do_validate:
         try:
-            result = sv.validate_string(request.body)
+            result = parse_tuple.validator.validate_string(request.body)
             if not result.valid:
                 raise StatusMessageException('0', ST_BAD_MESSAGE, 'Request was not schema valid: %s' % [err for err in result.error_log])
         except XMLSyntaxError as e:
             raise StatusMessageException('0', ST_BAD_MESSAGE, 'Request was not well-formed XML: %s' % str(e) )
-    
+
     try:
-        taxii_message = get_message_from_xml(request.body)
+        taxii_message = parse_tuple.parser(request.body)
     except tm11.UnsupportedQueryException as e:
         # TODO: Is it possible to give the real message id?
         # TODO: Is it possible to indicate which query aspects are supported?
         #       This might require a change in how libtaxii works
         raise StatusMessageException('0',
                                      ST_UNSUPPORTED_QUERY)
-    
+
     service = handlers.get_service_from_path(request.path)
-    #handler_class = handlers.get_message_handler(service, taxii_message)
     handler = service.get_message_handler(taxii_message)
     module_name, class_name = handler.handler.rsplit('.', 1)
-    
+
     try:
         module = import_module(module_name)
         handler_class = getattr(module, class_name)
     except Exception as e:
         type, value, tb = sys.exc_info()
         raise type, ("Error importing handler: %s" % handler.handler, type, value), tb
-   
+
     handler_class.validate_headers(request, taxii_message.message_id)
     handler_class.validate_message_is_supported(taxii_message)
-    
+
     try:
         response_message = handler_class.handle_message(service, taxii_message, request)
     except StatusMessageException:
@@ -79,11 +86,11 @@ def service_router(request, path, do_validate=True):
         msg = "There was a failure while executing the message handler"
         if settings.DEBUG: #Add the stacktrace
             msg += "\r\n" + traceback.format_exc()
-        
+
         raise StatusMessageException(taxii_message.message_id,
                                      ST_FAILURE,
                                      msg )
-    
+
     try:
         response_message.message_type
     except AttributeError as e:
@@ -96,15 +103,15 @@ def service_router(request, path, do_validate=True):
         raise StatusMessageException(taxii_message.message_id,
                                      ST_FAILURE,
                                      msg)
-    
+
     if response_message.__module__ == 'libtaxii.messages_11':
         vid = VID_TAXII_SERVICES_11
     elif response_message.__module__ == 'libtaxii.messages_10':
         vid = VID_TAXII_SERVICES_10
     else:
         raise ValueError("Unknown response message module")
-    
+
     response_headers = handlers.get_headers(vid, request.is_secure())
-        
+
     return handlers.HttpResponseTaxii(response_message.to_xml(pretty_print=True), response_headers)
     
