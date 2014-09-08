c63710a85fd12405622ca50cea8adaedd5447b1c
MarkDavidson <mdavidson@mitre.org>
2014-09-08 13:20:54 -0400

    commented out stuff to allow local sphinx building

--- docs/conf.py
+++ docs/conf.py
@@ -248,9 +248,9 @@ texinfo_documents = [
 #texinfo_no_detailmenu = False
 
 ## MSD 9/5/2014 - Set settings.py so that the build can succeed
-import os
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:
-    raise Exception("The RTD module can only be imported for readthedocs builds!")
+#import os
+#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#if not on_rtd:
+#    raise Exception("The RTD module can only be imported for readthedocs builds!")
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'rtd.settings'
