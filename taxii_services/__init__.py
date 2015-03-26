231aa38d66c55d154409916af7d52a51146a9ab2
Greg Back <gback@mitre.org>
2015-03-26 12:10:01 -0500

    Bump version to 0.4

--- taxii_services/__init__.py
+++ taxii_services/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2014 - The MITRE Corporation
 # For license information, see the LICENSE.txt file
 
-__version__ = "0.3"
+__version__ = "0.4"
 
 def register_admins(admin_list=None):
     """
